4d4bd902e708c9d1a11cdd0f58c03af51fe71f06
AnastasiyaYurc <yurcanastasiya@gmail.com>
2025-10-07 16:33:08 +0300

    feat: added modal.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,82 @@
- const getLoremImage = () => {
-     const images = document.querySelectorAll('img');
+window.addEventListener('load', () => {
+    const getLoremImage = () => {
+        const images = document.querySelectorAll('img');
 
-     images.forEach(item => {
-         if (!item.getAttribute('src')) {
-             fetch('https://picsum.photos/640/360')
-                 .then(response => response.blob())
-                 .then(blob => item.src = URL.createObjectURL(blob))
-                 .catch(error => item.src = "assets/img/cat-scottish.webp");
-         }
-     });
- };
+        images.forEach(item => {
+            if (!item.getAttribute('src')) {
+                fetch('https://picsum.photos/332/420')
+                    .then(response => response.blob())
+                    .then(blob => item.src = URL.createObjectURL(blob))
+                    .catch(error => item.src = "assets/img/cat-scottish.webp");
+            }
+        });
+    };
 
- const burgerBtn = document.querySelector('#burger');
- const mobileContainer = document.querySelector('#mobile-container')
- const body = document.querySelector('body')
+    const burgerBtn = document.querySelector('#burger');
+    const mobileContainer = document.querySelector('#mobile-container');
+    const body = document.querySelector('body')
 
- burgerBtn.addEventListener('click', ()=> {
-     mobileContainer.classList.toggle('hidden')
-     burgerBtn.classList.toggle('after:bg-purple')
-     burgerBtn.classList.toggle('before:bg-purple')
-     body.classList.toggle('overflow-hidden')
- })
+    burgerBtn.addEventListener('click', () => {
+        mobileContainer.classList.toggle('hidden')
+        burgerBtn.classList.toggle('after:bg-purple')
+        burgerBtn.classList.toggle('before:bg-purple')
+        body.classList.toggle('overflow-hidden')
+    })
 
- document.querySelectorAll('img').length > 0 ? getLoremImage() : null;
+
+    const installTabs = () => {
+        const tabsButtons = document.querySelectorAll('[data-tab-content]')
+        const tabContent = document.querySelectorAll('[tab-content]')
+
+        const removeActiveClassForNav = () => {
+            tabsButtons.forEach(item => item.classList.remove('bg-purple'))
+        }
+
+        const hiddenContentTabs = () => {
+            tabContent.forEach(item => item.classList.add('hidden'))
+        }
+
+        tabsButtons.forEach((item, index, array) => {
+            const tabsButton = item.getAttribute('data-tab-content');
+
+            item.addEventListener('click', () => {
+                const showContent = document.getElementById(tabsButton)
+                removeActiveClassForNav()
+                item.classList.add('bg-purple')
+
+                hiddenContentTabs()
+                showContent.classList.remove('hidden')
+            })
+        })
+    }
+
+
+//    let partnersSwiper;
+
+//    function initSwiper() {
+ //       if (window.innerWidth >= 768 && !partnersSwiper) {
+//            partnersSwiper = new Swiper(".swiper", {
+//                slidesPerView: 5,
+//                breakpoints: {
+//                    1024: {
+//                        slidesPerView: 4
+//                    },
+//                    768: {
+//                        slidesPerView: 6
+//                    },
+//                },
+//            });
+//        } else if (window.innerWidth < 768 && partnersSwiper) {
+//            partnersSwiper.destroy(true, true);
+//            partnersSwiper = null;
+//       }
+//    }
+
+//    window.addEventListener("load", initSwiper);
+  //  window.addEventListener("resize", initSwiper);
+
+
+
+    document.querySelectorAll('.tabs').length ? installTabs() : null;
+    document.querySelectorAll('img').length ? getLoremImage() : null;
+})
