e8becab45d93f20c4f7c536bd952901c36430b01
AnastasiyaYurc <yurcanastasiya@gmail.com>
2025-10-21 17:32:13 +0300

    feat: created dribbble and about section

--- src/modal.js
+++ src/modal.js
@@ -1,24 +1,18 @@
 window.addEventListener('load', () => {
-
     const initModalWindow = () => {
         const modalShowTrigers = document.querySelectorAll('[data-triger]')
         const modalCloseTriger = document.querySelectorAll('[data-close-modal]')
-
         modalShowTrigers.forEach(triger => {
             triger.addEventListener('click', () => {
                 const trigerData = triger.getAttribute('data-triger')
                 document.getElementById(trigerData).classList.add('show')
             })
-        })
-
+        }
+        )
         modalCloseTriger.forEach(button => {
             button.addEventListener('click', () => {
                 //     document.querySelector('.modal.show').classList.remove('show')
-
                 button.closest('.modal').classList.remove('show')
-
-
-
             })
         })
 
