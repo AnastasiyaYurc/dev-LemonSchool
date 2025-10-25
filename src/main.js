ba97fdc30108a70319071c3a9c065a6e539623c7
AnastasiyaYurc <yurcanastasiya@gmail.com>
2025-10-25 09:57:35 +0300

    fix: partners slides

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', () => {
-
     const burgerBtn = document.querySelector('#burger');
     const mobileContainer = document.querySelector('#mobile-container');
     const body = document.querySelector('body')
@@ -13,55 +12,46 @@ window.addEventListener('load', () => {
 
     const installTabs = () => {
         const tabsButtons = document.querySelectorAll('[data-tab-content]')
-        const tabContent = document.querySelectorAll('[tab-content]')
+        const tabContent = document.querySelectorAll('.tab-content')
 
         const removeActiveClassForNav = () => {
             tabsButtons.forEach(item => item.classList.remove('bg-purple'))
         }
-
         const hiddenContentTabs = () => {
             tabContent.forEach(item => item.classList.add('hidden'))
         }
-
         tabsButtons.forEach((item, index, array) => {
             const tabsButton = item.getAttribute('data-tab-content');
-
             item.addEventListener('click', () => {
                 const showContent = document.getElementById(tabsButton)
                 removeActiveClassForNav()
                 item.classList.add('bg-purple')
-
                 hiddenContentTabs()
                 showContent.classList.remove('hidden')
             })
         })
     }
 
+    let partnersSwiper;
+
+    partnersSwiper = new Swiper(".swiper", {
+        slidesPerView: 5,
+    })
+
+    const initSwiper = () => {
+        if (window.innerWidth >= 992 && partnersSwiper) {
+            new Swiper(".swiper", {
+                slidesPerView: 5,
+            });
+
+        } else if (window.innerWidth < 991 && partnersSwiper) {
+            partnersSwiper.destroy(true, true);
+            partnersSwiper = null;
+        }
+    }
+
+    window.addEventListener("load", initSwiper);
+    window.addEventListener("resize", initSwiper);
 
     document.querySelectorAll('.tabs').length ? installTabs() : null;
 })
-
-
-//    let partnersSwiper;
-
-//    function initSwiper() {
- //       if (window.innerWidth >= 768 && !partnersSwiper) {
-//            partnersSwiper = new Swiper(".swiper", {
-//                slidesPerView: 5,
-//                breakpoints: {
-//                    1024: {
-//                        slidesPerView: 4
-//                    },
-//                    768: {
-//                        slidesPerView: 6
-//                    },
-//                },
-//            });
-//        } else if (window.innerWidth < 768 && partnersSwiper) {
-//            partnersSwiper.destroy(true, true);
-//            partnersSwiper = null;
-//       }
-//    }
-
-//    window.addEventListener("load", initSwiper);
-  //  window.addEventListener("resize", initSwiper);
